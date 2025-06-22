4e39794814d39199b8c616c9727c0970f6f353be
Blippy <blippy@duck.com>
2025-06-22 03:44:09 -0700

    Added .github directory, .github/workflows directory, .github/workflows/Setup .NET Core SDK file, .github/workflows/jekyll-docker.yml file, Ballymoney.jpg image file, updated README.md file, improved index.html file, mv styles.css to style.css and improve the style.css file, added the script.js file.
    Merge merge

---
+++ script.js
@@ -0,0 +1,20 @@
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Parallax effect (doesn't work)
+//document.addEventListener('scroll', function () {
+//    const parallaxElements = document.querySelectorAll('.parallax-1, .parallax-2');
+//    parallaxElements.forEach(element => {
+//        const speed = element.getAttribute('data-speed');
+//        const yPos = -((window.scrollY * speed) / 100);
+//        element.style.transform = `translateY(${yPos}px)`;
+//    });
+//});
